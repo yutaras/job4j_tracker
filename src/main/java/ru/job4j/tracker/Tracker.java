9fd02bb2866a84db28bbdaff387fdfd39f953a36
Yuriy Tarasenko <maxjeff@mail.ru>
2021-05-07 17:11:27 +0300

    7. Метод удаления заявки Tracker.delete [#505366]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -63,7 +63,7 @@ public class Tracker {
         boolean rsl = false;
         int index = indexOf(id);
         if (index != -1) {
-            System.arraycopy(items, index + 1, items, index, size - index);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             rsl = true;
