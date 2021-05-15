6d732b8925fa7d7312b44ad166a82d00acba8c1e
Yuriy Tarasenko <maxjeff@mail.ru>
2021-05-15 17:28:08 +0300

    3. Интерфейс Input [#505286]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,12 +3,12 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("Водить");
     }
 
     @Override
     public void passengers(int countPassengers) {
-
+        System.out.println("Количетво пассажиров");
     }
 
     @Override
