de7fcb38678f28132a6679a99e7c7a62fc78febf
Yuriy Tarasenko <maxjeff@mail.ru>
2021-05-30 15:47:21 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -98,7 +98,8 @@ public class StartUITest {
                         "Menu." + System.lineSeparator() +
                         "0. Show all items" + System.lineSeparator() +
                         "1. Exit Program" + System.lineSeparator()
-        ));;
+        ));
+        ;
     }
 
     @Test
@@ -124,7 +125,7 @@ public class StartUITest {
                         "Menu." + System.lineSeparator() +
                         "0. Find item by id" + System.lineSeparator() +
                         "1. Exit Program" + System.lineSeparator()
-                ));
+        ));
     }
 
     @Test
@@ -151,6 +152,28 @@ public class StartUITest {
                         "0. Find item by name" + System.lineSeparator() +
                         "1. Exit Program" + System.lineSeparator()
         ));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"7", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitProgramAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu." + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu." + ln
+                        + "0. Exit Program" + ln
+                )
+        );
     }
 
     @Test
