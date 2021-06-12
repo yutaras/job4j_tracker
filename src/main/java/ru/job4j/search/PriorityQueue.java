25f38c87c8d9ffbf985fa22320932cb918f8f1a1
Yuriy Tarasenko <maxjeff@mail.ru>
2021-06-12 10:55:32 +0300

    2. Очередь с приоритетом на LinkedList [#41670]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -15,7 +15,7 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() <= element.getPriority()) {
+            if (task.getPriority() < element.getPriority()) {
                 break;
             }
             index++;
