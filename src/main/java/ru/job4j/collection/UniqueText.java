bb80a05fb891d0198448437c590c428bdfd51e0c
Yuriy Tarasenko <maxjeff@mail.ru>
2021-06-19 18:05:54 +0300

    2. Идентичные тексты [#212655]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,20 +4,20 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = false;
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
         /* for-each origin -> new HashSet. */
-        for (String word1 : origin) {
-            check.add(word1);
+        for (String wordsOfOrigin : origin) {
+            check.add(wordsOfOrigin);
         }
         /* for-each text -> hashSet.contains */
-        for (String word2 : text) {
-            if (check.contains(word2)) {
-                rsl = true;
-            }
+        for (String wordsOfText : text) {
+            if (!check.contains(wordsOfText)) {
+                rsl = false;
                 break;
+            }
         }
         return rsl;
     }
