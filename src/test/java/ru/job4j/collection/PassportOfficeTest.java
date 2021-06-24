c571b5a9fec6c9851c3cffd8a1496aec274e8a18
Yuriy Tarasenko <maxjeff@mail.ru>
2021-06-24 21:47:29 +0300

    1. Паспорт и жители [#212656 #171010]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -10,6 +10,14 @@ public class PassportOfficeTest {
     @Test
     public void add() {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
