ae81be626237728bc909c670cb7cb48fdd72cc7f
Yuriy Tarasenko <maxjeff@mail.ru>
2021-07-24 14:02:28 +0300

    4. Optional в Stream API. [#279130]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * Класс описывает работу банковского сервиса
+ *
  * @author YURI TARASENKO
  * @version 1.0
  */
@@ -15,10 +16,11 @@ public class BankService {
 
     /**
      * метод должен добавить пользователя в систему
+     *
      * @param user пользователь, который добавляется в систему
      */
     public void addUser(User user) {
-     users.putIfAbsent(user, new ArrayList<>());
+        users.putIfAbsent(user, new ArrayList<>());
     }
 
     /**
@@ -26,13 +28,14 @@ public class BankService {
      * по паспорту с помощ метод findByPassport.После этого мы получяем список
      * всех счетов пользователя и добавим новый счет к ним. В этом методе
      * есть проверка, что такого счета у пользователя еще нет.
+     *
      * @param passport паспорт пользователя
-     * @param account счет пользователя
+     * @param account  счет пользователя
      */
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = users.get(user);
+        Optional<User> user = findByPassport(passport);
+        if (user.isPresent()) {
+            List<Account> accounts = users.get(user.get());
             if (!accounts.contains(account)) {
                 accounts.add(account);
             }
@@ -41,15 +44,15 @@ public class BankService {
 
     /**
      * Метод ищет пользователя по номеру паспорта
+     *
      * @param passport паспорт пользователя
      * @return возвращает пользователя или null, если он не найден
      */
-    public User findByPassport(String passport) {
+    public Optional<User> findByPassport(String passport) {
         return users.keySet()
                 .stream()
                 .filter(s -> s.getPassport().equals(passport))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
     /*public User findByPassport(String passport) {
         User rsl = null;
@@ -65,20 +68,17 @@ public class BankService {
     /**
      * Этот метод ищет счет пользователя по реквизитам. Сначала находим пользователя.
      * Потом получаем список счетов этого пользователя и в нем находим нужный счет.
-     * @param passport паспорт пользователя
+     *
+     * @param passport  паспорт пользователя
      * @param requisite реквизиты пользователя
      * @return возвращает счет пользователя или null, если счет не найден
      */
-    public Account findByRequisite(String passport, String requisite) {
-        User a = findByPassport(passport);
-        if (a != null) {
-            return users.get(a)
-                    .stream()
-                    .filter(s -> s.getRequisite().equals(requisite))
-                    .findFirst()
-                    .orElse(null);
-        }
-        return null;
+    public Optional<Account> findByRequisite(String passport, String requisite) {
+        Optional<User> a = findByPassport(passport);
+        return a.flatMap(user -> users.get(user)
+                .stream()
+                .filter(s -> s.getRequisite().equals(requisite))
+                .findFirst());
     }
     /*public Account findByRequisite(String passport, String requisite) {
         Account rsl = null;
@@ -98,20 +98,22 @@ public class BankService {
      * Метод для перечисления денег с одного счёта на другой счёт.
      * Если счёт не найден или не хватает денег на счёте srcAccount (с которого переводят),
      * то метод должен вернуть false
-     * @param srcPassport паспорт источника
-     * @param srcRequisite реквизиты источника
-     * @param destPassport паспорт получателя
+     *
+     * @param srcPassport   паспорт источника
+     * @param srcRequisite  реквизиты источника
+     * @param destPassport  паспорт получателя
      * @param destRequisite реквизиты получателя
-     * @param amount доступные средства на счете источника
+     * @param amount        доступные средства на счете источника
      * @return true если перевод выполнится или false, если не возможен.
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
-        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
-        Account destAcc = findByRequisite(destPassport, destRequisite);
-        if ((srcAcc != null) && (destAcc != null) && (srcAcc.getBalance() >= amount)) {
-            srcAcc.setBalance(srcAcc.getBalance() - amount);
-            destAcc.setBalance(destAcc.getBalance() + amount);
+        Optional<Account> srcAcc = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> destAcc = findByRequisite(destPassport, destRequisite);
+        if ((srcAcc.isPresent()) && (destAcc.isPresent())
+                && (srcAcc.get().getBalance() >= amount)) {
+            srcAcc.get().setBalance(srcAcc.get().getBalance() - amount);
+            destAcc.get().setBalance(destAcc.get().getBalance() + amount);
             return true;
         }
         return false;
--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.bank;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import java.util.Optional;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -9,19 +12,19 @@ public class BankServiceTest {
 
     @Test
     public void addUser() {
-        User user = new User("3434", "Petr Arsentev");
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
         BankService bank = new BankService();
-        bank.addUser(user);
+        bank.addUser(user.get());
         assertThat(bank.findByPassport("3434"), is(user));
     }
 
     @Test
     public void whenEnterInvalidPassport() {
-        User user = new User("3434", "Petr Arsentev");
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
         BankService bank = new BankService();
-        bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertNull(bank.findByRequisite("34", "5546"));
+        bank.addUser(user.get());
+        bank.addAccount(user.get().getPassport(), new Account("5546", 150D));
+        assertThat(bank.findByRequisite("34", "5546"), is(Optional.empty()));
     }
 
     @Test
@@ -30,7 +33,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
+        assertThat(bank.findByRequisite("3434", "5546").get().getBalance(), is(150D));
     }
 
     @Test
@@ -41,6 +44,6 @@ public class BankServiceTest {
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
         bank.addAccount(user.getPassport(), new Account("113", 50D));
         bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
-        assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
+        assertThat(bank.findByRequisite(user.getPassport(), "113").get().getBalance(), is(200D));
     }
 }
